35cfc41843fb3c8c46fd98ea72526966aac64a7d
domonik <schdruzzi@gmail.com>
2021-02-18 16:55:39 +0100

    dark template begins

--- interactionDashboard.py
+++ interactionDashboard.py
@@ -5,20 +5,35 @@ import pickle
 import dash  # (version 1.12.0) pip install dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import numpy as np
-app = dash.Dash(__name__)
+import plotly.io as pio
+
+app = dash.Dash(__name__, )
 
 filter_columns = ["input_min", "input_max", "filter_value", "filter_size", "cutoff"]
 PLOTLY_COLORS = px.colors.qualitative.Dark24
+pio.templates["plotly_dark_custom"] = pio.templates["plotly_dark"]
+
+templa = pio.templates["plotly_dark_custom"].update({"layout": {
+    # e.g. you want to change the background to transparent
+    'paper_bgcolor': 'rgba(0,0,0,0)',
+    'plot_bgcolor': '#E5ECF6'
+}})
+
 
 def get_table(filter_columns):
     table = html.Table(
         [html.Tr([html.Td(col) for col in filter_columns])] +
         [html.Tr(
-            [html.Td(dcc.Input(id="{}".format(inp), type="number", placeholder="{}".format(inp))) for inp in filter_columns])]
+            [html.Td(dcc.Input(id="{}".format(inp), type="number", placeholder="{}".format(inp))) for inp in
+             filter_columns] +
+            [html.Button("Switch mode", id="switch_button", n_clicks=0)]
+        )]
     )
     return table
+
 
 with open("plotly_visualize_DATA_200mid.pickle", "rb") as f:
     df = pickle.load(f)
@@ -36,7 +51,6 @@ for entry in selection:
     if ct >= len(PLOTLY_COLORS):
         ct = 0
 
-types = ("number", "text")
 
 app.layout = html.Div([
 
@@ -50,12 +64,13 @@ app.layout = html.Div([
                  ),
     html.Div(id='output_container', children=[]),
     html.Br(),
-    dcc.Graph(id='plotly_graph', figure={"layout":{"height": 700}}),
+    dcc.Graph(id='plotly_graph', figure={"layout": {"height": 700}}),
     html.Br(),
     get_table(filter_columns),
 
 ],
 )
+
 
 @app.callback(
     [Output(component_id='output_container', component_property='children'),
@@ -66,10 +81,12 @@ app.layout = html.Div([
      Input(component_id="filter_value", component_property="value"),
      Input(component_id="filter_size", component_property="value"),
      Input(component_id="cutoff", component_property="value"),
+     Input(component_id="switch_button", component_property="n_clicks"),
      ]
 )
-def update_graph(option_slctd, inputmin, inputmax, filter_value, filter_size, cutoff):
+def update_graph(option_slctd, inputmin, inputmax, filter_value, filter_size, cutoff, switch):
     container = "The protein chosen by user was: {}".format(option_slctd)
+    viewmode = switch % 2
     if filter_value is None:
         filter_value = 0
     if inputmin is None:
@@ -86,20 +103,28 @@ def update_graph(option_slctd, inputmin, inputmax, filter_value, filter_size, cu
     fig = go.Figure()
     to_plot = []
     for i in range(len(dff)):
-        y = dff["normalized_interactions"].iloc[i]
-        name = dff["protein2"].iloc[i]
+        if viewmode == 0:
+            y = dff["normalized_interactions"].iloc[i]
+        else:
+            y = dff["interactions"].iloc[i]
         num_i = dff["interactions"].iloc[i][-1]
+        name = dff["protein2"].iloc[i]
+        name_ext = dff["protein2"].iloc[i] + ":" + str(num_i)
 
         diff = dff["differences"].iloc[i]
         diff_window = get_sum(diff, filter_size)
         max_diff = max(diff_window[cutoff:])
         if inputmax >= num_i >= inputmin:
             if max_diff > filter_value:
-                to_plot.append(go.Scatter(x=x_axis, y=y, mode="lines", name=name, hovertext=name, line={"width": 4, "color": colordict[name]}))
+                to_plot.append(go.Scatter(x=x_axis, y=y, mode="lines", name=name_ext, hovertext=name_ext,
+                                          line={"width": 4, "color": colordict[name]}))
             else:
-                fig.add_trace(go.Scatter(x=x_axis, y=y, mode="lines", name=name, hovertext=name, line={"color": "grey"}))
+                fig.add_trace(go.Scatter(x=x_axis, y=y, mode="lines", name=name_ext, hovertext=name_ext,
+                                         line={"color": "grey"}))
     for element in to_plot:
         fig.add_trace(element)
+    # fig.layout.template = "plotly_dark_custom"
+    fig.update_layout(hoverlabel=dict(namelength=0))
     return container, fig
 
 
@@ -116,7 +141,6 @@ def get_sum(arr, dif):
         med = np.sum(area)
         result_array[x] = med
     return result_array
-
 
 
 if __name__ == '__main__':
