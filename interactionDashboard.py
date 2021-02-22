85a9ab9685f75a10fd7285d0c274ce9a68b96fde
domonik <schdruzzi@gmail.com>
2021-02-22 16:58:15 +0100

    added basic css

--- interactionDashboard.py
+++ interactionDashboard.py
@@ -10,27 +10,31 @@ from dash.dependencies import Input, Output
 import numpy as np
 import plotly.io as pio
 
-app = dash.Dash(__name__, )
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
 filter_columns = ["input_min", "input_max", "filter_value", "filter_size", "cutoff"]
-PLOTLY_COLORS = px.colors.qualitative.Dark24
+PLOTLY_COLORS = px.colors.qualitative.Light24
+del PLOTLY_COLORS[12]
 pio.templates["plotly_dark_custom"] = pio.templates["plotly_dark"]
 
-templa = pio.templates["plotly_dark_custom"].update({"layout": {
+templa = pio.templates["plotly_white"].update({"layout": {
     # e.g. you want to change the background to transparent
     'paper_bgcolor': 'rgba(0,0,0,0)',
-    'plot_bgcolor': '#E5ECF6'
+    'plot_bgcolor': ' rgba(0,0,0,0)',
+    "font": dict(color="white")
+
 }})
 
 
 def get_table(filter_columns):
+
     table = html.Table(
-        [html.Tr([html.Td(col) for col in filter_columns])] +
+        [html.Tr([html.Th(html.Div(col, className="table-header")) for col in filter_columns])] +
         [html.Tr(
-            [html.Td(dcc.Input(id="{}".format(inp), type="number", placeholder="{}".format(inp))) for inp in
+            [html.Td(html.Div(dcc.Input(id="{}".format(inp), type="number", placeholder="{}".format(inp), style={"width": "100%"})) , className="filter_table_interactive") for inp in
              filter_columns] +
-            [html.Button("Switch mode", id="switch_button", n_clicks=0)]
-        )]
+            [html.Div(dbc.Button("Switch mode", id="switch_button", color="light", n_clicks=0, style={"white-space": "nowrap", "margin": "auto"}), className="filter_table_interactive")]
+        )], id="filter-table", style={"margin": "auto", "width": "80%"}
     )
     return table
 
@@ -40,6 +44,7 @@ with open("plotly_visualize_DATA_200mid.pickle", "rb") as f:
 x_axis = np.array(list(range(len(df["normalized_interactions"].iloc[0])))) * 5
 selection = sorted(df["protein1"].unique())
 opts = []
+opts2 = []
 
 colordict = dict()
 ct = 0
@@ -47,6 +52,7 @@ for entry in selection:
     colordict[entry] = PLOTLY_COLORS[ct]
     opt = {"label": entry, "value": entry}
     opts.append(opt)
+    opts2.append(dbc.DropdownMenuItem(entry, id=entry))
     ct += 1
     if ct >= len(PLOTLY_COLORS):
         ct = 0
@@ -54,21 +60,22 @@ for entry in selection:
 
 app.layout = html.Div([
 
-    html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
-
-    dcc.Dropdown(id="slct_year",
+    html.Div(html.H1("Protein Interaction Dasboard", style={'text-align': 'center'}), className="page-header"),
+    #dbc.DropdownMenu(id="dbcdd", children=opts2),
+    html.Div(dcc.Dropdown(id="slct_year",
                  options=opts,
                  multi=False,
                  value=selection[0],
-                 style={'width': "70%"}
-                 ),
-    html.Div(id='output_container', children=[]),
-    html.Br(),
-    dcc.Graph(id='plotly_graph', figure={"layout": {"height": 700}}),
-    html.Br(),
-    get_table(filter_columns),
+                 style={'width': "50%", "margin": "auto"}
+                 ), className="databox"),
 
-],
+    html.Div([html.H2(id='output_container', children=[], style={"text-align": "center"}),
+        dcc.Graph(id='plotly_graph', figure={"layout": {"height": 700}}),
+              get_table(filter_columns)], className="plotly-graph"),
+    html.Br(),
+
+
+], id="wrapper"
 )
 
 
@@ -85,7 +92,7 @@ app.layout = html.Div([
      ]
 )
 def update_graph(option_slctd, inputmin, inputmax, filter_value, filter_size, cutoff, switch):
-    container = "The protein chosen by user was: {}".format(option_slctd)
+    container = "{} Interactions".format(option_slctd)
     viewmode = switch % 2
     if filter_value is None:
         filter_value = 0
@@ -123,7 +130,7 @@ def update_graph(option_slctd, inputmin, inputmax, filter_value, filter_size, cu
                                          line={"color": "grey"}))
     for element in to_plot:
         fig.add_trace(element)
-    # fig.layout.template = "plotly_dark_custom"
+    fig.layout.template = "plotly_white"
     fig.update_layout(hoverlabel=dict(namelength=0))
     return container, fig
 
@@ -133,6 +140,7 @@ def get_sum(arr, dif):
     for x in range(len(arr)):
         window = (x - dif, x + dif + 1)
         if window[0] < 0:
+
             area = arr[0:window[1]]
         elif window[1] > len(arr):
             area = arr[window[0]:]
